7cb375e9eebd4a18bd63b1f11f3398f9d0e46556
Javier Feliu <jfeliu@nuvanz.com>
2018-01-31 21:33:18 -0500

    Add resource for employment_position

---
+++ resources/employment_position.py
@@ -0,0 +1,129 @@
+from flask_jwt import jwt_required
+from flask_restful import Resource, reqparse
+from sqlalchemy import exc
+
+from models.employment_position import EmploymentPositionModel
+
+
+class EmploymentPosition(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('position_name_feminine',
+                        type=str,
+                        required=True,
+                        help='This field cannot be blank.')
+    parser.add_argument('position_name_masculine',
+                        type=str,
+                        required=True,
+                        help='This field cannot be blank.')
+    parser.add_argument('minimun_hourly_wage',
+                        type=float,
+                        required=False,
+                        help='This field must be a number.')
+    parser.add_argument('is_active',
+                        default=True,
+                        type=bool,
+                        required=False)
+    parser.add_argument('organization_id',
+                        type=int,
+                        required=True,
+                        help='A deparment must belong to an organization')
+
+    @jwt_required()
+    def get(self, position_name, organization_id):
+        e_p = EmploymentPositionModel.find_by_name(position_name,
+                                                   organization_id)
+        if e_p:
+            return e_p.to_dict()
+        return {'message': 'Employment position not found'}, 404
+
+    @staticmethod
+    @jwt_required()
+    def post():
+        data = EmploymentPosition.parser.parse_args()
+
+        if EmploymentPositionModel\
+                .find_by_name(data['position_name_feminine'],
+                              data['organization_id']) or \
+           EmploymentPositionModel\
+                .find_by_name(data['position_name_masculine'],
+                              data['organization_id']):
+            return {'message': 'An employment position with that name already '
+                               'exists in the organization'}, 400
+
+        e_p = EmploymentPositionModel(data['position_name_feminine'],
+                                      data['position_name_masculine'],
+                                      data['minimun_hourly_wage'],
+                                      data['is_active'],
+                                      data['organization_id'])
+
+        try:
+            e_p.save_to_db()
+        except exc.SQLAlchemyError:
+            return {'message': 'An error occurred creating '
+                               'the employment position.'}, 500
+
+        return {'message': 'Department created successfully.'}, 201
+
+    @jwt_required()
+    def put(self,  position_name, organization_id):
+        data = EmploymentPosition.parser.parse_args()
+
+        e_p = EmploymentPositionModel.find_by_name(position_name,
+                                                   organization_id)
+
+        if e_p:
+            e_p.department_name = data['position_name_feminine']
+            e_p.department_name = data['position_name_masculine']
+            e_p.department_name = data['minimun_hourly_wage']
+
+            try:
+                e_p.save_to_db()
+                return {'message': 'Employment position updated '
+                                   'successfully.'}, 200
+            except exc.SQLAlchemyError:
+                return {'message': 'An error occurred updating '
+                                   'the employment position.'}, 500
+        else:
+            return {'message': 'Employment position not found'}, 404
+
+    @jwt_required()
+    def delete(self, position_name, organization_id):
+        e_p = EmploymentPositionModel.find_by_name(position_name,
+                                                   organization_id)
+
+        if e_p:
+            if e_p.is_active:
+                try:
+                    e_p.inactivate()
+                    return {'message': 'Employment position is '
+                                       'now inactive.'}, 200
+                except exc.SQLAlchemyError:
+                    return {'message': 'An error occurred while inactivating'
+                                       'the employment position.'}, 500
+            else:
+                return {'message': 'Employment position was '
+                                   'already inactive.'}, 400
+        else:
+            return {'message': 'Employment position not found'}, 404
+
+
+class ActivateEmploymentPosition(Resource):
+    @jwt_required()
+    def put(self, position_name, organization_id):
+        e_p = EmploymentPositionModel.find_by_name(position_name,
+                                                   organization_id)
+
+        if e_p:
+            if not e_p.is_active:
+                try:
+                    e_p.activate()
+                    return {'message': 'Employment position is '
+                                       'now active.'}, 200
+                except exc.SQLAlchemyError:
+                    return {'message': 'An error occurred while activating'
+                                       'the employment position.'}, 500
+            else:
+                return {'message': 'Employment position was '
+                                   'already active.'}, 400
+        else:
+            return {'message': 'Employment position not found'}, 404
